254fe0b0a80021ae397fe093150ad084e8d8b1a1
Yuxuan Jiang <jiangy10@rose-hulman.edu>
2024-04-11 15:50:12 +0800

    facade check refactor

--- src/main/java/domain/check/FacadeCheck.java
+++ src/main/java/domain/check/FacadeCheck.java
@@ -12,39 +12,42 @@ import java.util.List;
 
 public class FacadeCheck extends Check{
     private static final int DEPENDENCY_THRESHOLD = 5;
+    private List<Violation> violations = new ArrayList<Violation>();
 
     public FacadeCheck(List<ClassData> classes, List<Relation> relations) {super(classes, relations);}
 
     @Override
     public List<Violation> checker() {
-        List<Violation> violations = new ArrayList<>();
-        for(ClassData classData : classes){
-            List<MethodData> methodDatas = classData.getMethods();
-            boolean isMain = false;
-
-            for(MethodData methodData : methodDatas){ //Find which class is main, main cannot be a facade
-                if (methodData.getName().contains("main") &&
-                        methodData.getAccessModifier() == AccessModifiers.PUBLIC &&
-                        methodData.isStatic()){ //Check for public methods called main
-                    isMain = true;
-                }
-            }
-
-            if(!isMain){//Check not main for calling several methods from different classes
-                int deps = 0;
-                for(Relation relation : relations){
-                    if(relation.getFirstClass().equals(classData.getName()) &&
-                            relation.getRelationship() == RelationshipTypes.USES){
-                        deps ++;
-                    }
-                }
-                if(deps >= DEPENDENCY_THRESHOLD){
-                    Violation violation = new Violation("Facade found",
-                            classData.getName());
-                    violations.add(violation);
-                }
+        for(ClassData classData : classes) {
+            if (!isMainClass(classData)) {
+                checkForFacade(classData);
             }
         }
         return violations;
+    }
+
+    private boolean isMainClass(ClassData classData) {
+        for(MethodData methodData : classData.getMethods()) {
+            if (methodData.getName().contains("main") &&
+                    methodData.getAccessModifier() == AccessModifiers.PUBLIC &&
+                    methodData.isStatic()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void checkForFacade(ClassData classData) {
+        int deps = 0;
+        for(Relation relation : relations) {
+            if (relation.getFirstClass().equals(classData.getName()) &&
+                    relation.getRelationship() == RelationshipTypes.USES) {
+                deps++;
+            }
+        }
+        if (deps >= DEPENDENCY_THRESHOLD) {
+            Violation violation = new Violation("Facade found", classData.getName());
+            violations.add(violation);
+        }
     }
 }
